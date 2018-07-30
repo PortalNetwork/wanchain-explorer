3935f8677df78b03507d192a1a80c7de89becba3
MikeCheng1208 <a3804430@hotmail.com>
2018-07-30 21:22:36 +0800

    Features

--- src/components/App.js
+++ src/components/App.js
@@ -3,38 +3,98 @@ import SearchItem from "./SearchItem";
 import { getContent } from '../lib/resolverService';
 import { getResolver } from '../lib/registryService';
 import { fromContentHash } from '../helpers/ipfsHelper';
+import { getEntries } from '../lib/registrarService';
+import { getOwner } from '../lib/deedService';
 import "./App.scss";
+import Loading from './Loading';
 class App extends Component {
 
 
-    handSeachitem = e =>{
-        if(e.keyCode !== 13) return;
-        
+    state = {
+        seachValue : "",
+        entries: {},
+        w: {},
+        isKeyDown: false,
+        idxRes: 0,
+        isOpenSearch: false,
     }
-    
-    componentDidMount(){
-        getResolver('game.portalnetworkweb.wan').then(resolver => {
-            getContent('game.portalnetworkweb.wan', resolver).then(contentHash => {
-                console.log("contentHash:",contentHash, fromContentHash(contentHash));
+
+    handleInputChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    handSeachitem = e =>{
+        if(this.state.isKeyDown) return;
+        if(e.keyCode !== 13) return;
+        const keydomain = this.state.seachValue.split(".wan");
+        if(keydomain[keydomain.length - 1] !== "") return;
+        const domain = keydomain[keydomain.length - 2].split(".");
+        const seachdamain = domain[domain.length-1];    //去頭去尾去.wan
+        this.setState({isKeyDown: true, isOpenSearch: false});
+        getEntries(seachdamain).then(entries => {
+            getOwner(entries.deed).then(owner => {
+                let t = this.state.idxRes+=1;
+                let eObj = entries;
+                eObj['owner'] = owner;
+                this.setState({
+                    entries: eObj,
+                    idxRes: t
+                },()=>this.overResolver())
             });
         });
-        
+        getResolver(`${seachdamain}.wan`).then(resolver => {
+            getContent(`${seachdamain}.wan`, resolver).then(contentHash => {
+                let t = this.state.idxRes+=1;
+                let rObj={ resolver, IPFSHash: `https://ipfs.infura.io/ipfs/${fromContentHash(contentHash)}`}
+                this.setState({
+                    content: rObj,
+                    idxRes: t
+                },()=>this.overResolver())
+            });
+        });
     }
+    overResolver =()=>{
+        if(this.state.idxRes !== 2) return;
+        this.setState({
+            isKeyDown: false,
+            isOpenSearch: true,
+            idxRes: 0,
+        })
+    }
+
+
     render() {
         return (
             <div className="wanchain">
-                <h1>WANChain Explorer</h1>
+                <h1>Wanchain Explorer</h1>
                 <div className="seach">
-                    <input type="text" onKeyDown={this.handSeachitem} />
+                    <input type="text" 
+                        onKeyDown={this.handSeachitem} 
+                        name="seachValue"
+                        value={this.state.seachValue}
+                        onChange={this.handleInputChange}
+                    />
                     <a 
                         onClick={this.handSeachitem} 
                         className="seach_icon"
                     ></a>
                 </div>
-                <SearchItem/>
+                { this.state.isKeyDown && <Loading/> }
+                <SearchItem
+                    seachValue={this.state.seachValue}
+                    isOpenSearch={this.state.isOpenSearch}
+                    entries={this.state.entries}
+                    content={this.state.content}
+                />
                 <span className="text">
                     Powered by <a href="https://www.portal.network/" target="_blank">Portal Network</a>
                 </span>
+
+                <div className="urllink">
+                    <a href="https://t.me/portalnetworkofficial" target="_blank"><i className="fab fa-telegram fa-2x"></i></a>
+                    <a href="https://github.com/PortalNetwork/wanchain-explorer" target="_blank"><i className="fab fa-github fa-2x"></i></a>
+                </div>
             </div>
         )
     }
